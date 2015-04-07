1475291d44f23eaf8fb1cb3736f79a5191db1b1b
Thi Duong <dathi@tma.com.vn>
2015-04-07 11:41:53 +0700

    update code 111

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,5 @@
 class ReportsController < ApplicationController
-  before_action :require_user
+  #before_action :require_user
   before_action :set_report, only: [:show, :edit, :update, :destroy]
 
   # GET /reports
